f1d50d4a49dddf7f925ee973834f8eb8147bd22b
Roik Sergei <roiksg@mail.ru>
2022-08-19 12:07:09 +0300

    Add Unwind Segue

--- AniLease/Modules/Main/ViewController.swift
+++ AniLease/Modules/Main/ViewController.swift
@@ -29,6 +29,10 @@ class ViewController: UIViewController {
             }
         }
     }
+    
+    @IBAction func unwindMain(_ unwindSegue: UIStoryboardSegue) {
+        
+    }
 }
 
 // MARK: extension
